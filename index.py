c188a9a63974f9d4ed2f01d780ecf484ed63ceab
nicollegordillo <nicollegordillo912@gmail.com>
2025-04-22 19:26:49 -0600

    fix: no magnum

--- index.py
+++ index.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 from routes.ordenes import router as ordenes_router
 from routes.resenias import router as resenias_router
 from routes.imagenes import router as imagenes_router
-from mangum import Mangum
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = FastAPI()
 
@@ -17,7 +13,5 @@ app.include_router(imagenes_router, prefix="/imagenes", tags=["Imagenes"])
 async def hello():
     return {"mensaje": "Hola desde FastAPI + MongoDB + Vercel"}
 
-# Este es el handler que Vercel necesita
-handler = Mangum(app)
 
 
