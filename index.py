ebc63a46a04c78aa8ce300cdd6194baf04c26d68
nicollegordillo <nicollegordillo912@gmail.com>
2025-04-22 20:15:52 -0600

    fix: index

--- index.py
+++ index.py
@@ -1,23 +1,15 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 from routes.ordenes import router as ordenes_router
 from routes.resenias import router as resenias_router
 from routes.imagenes import router as imagenes_router
 from database import create_db
 
-db = None  # para compartirlo entre rutas
+app = FastAPI()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    from database import create_db
-    db = await create_db()
-    app.state.db = db  # Lo asignas aquí, no como variable global
-    yield
-    # Puedes cerrar conexión si lo necesitas, ej. `await db.client.close()`
+@app.on_event("startup")
+async def startup_db():
+    app.state.db = await create_db()
 
-app = FastAPI(lifespan=lifespan)
-
-# Montar routers
 app.include_router(ordenes_router, prefix="/ordenes", tags=["Ordenes"])
 app.include_router(resenias_router, prefix="/resenias", tags=["Resenias"])
 app.include_router(imagenes_router, prefix="/imagenes", tags=["Imagenes"])
@@ -25,6 +17,7 @@ app.include_router(imagenes_router, prefix="/imagenes", tags=["Imagenes"])
 @app.get("/")
 async def hello():
     return {"mensaje": "Hola desde FastAPI + MongoDB + Vercel"}
+
 
 
 
