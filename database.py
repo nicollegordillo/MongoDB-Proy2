9a44605cd98104626efa3d0d3dc49ebaafa83ea3
nicollegordillo <nicollegordillo912@gmail.com>
2025-04-22 20:34:19 -0600

    Fix: async database

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
 
+@asynccontextmanager
 async def create_db():
     MONGO_URI = os.environ.get("MONGODB_URI")
     client = AsyncIOMotorClient(MONGO_URI)
