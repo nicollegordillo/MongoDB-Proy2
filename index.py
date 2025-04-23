83173b2cd5fc1fe8a65af1610121850cac06cabe
nicollegordillo <nicollegordillo912@gmail.com>
2025-04-23 08:35:23 -0600

    fix: depricated

--- index.py
+++ index.py
@@ -1,19 +1,25 @@
+# index.py
+import os
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
+
 from routes.ordenes import router as ordenes_router
 from routes.resenias import router as resenias_router
 from routes.imagenes import router as imagenes_router
-from database import startup_db, shutdown_db, get_db
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print(">> Lifespan inicializando...")
+    mongo_uri = os.environ.get("MONGODB_URI")
+    client = AsyncIOMotorClient(mongo_uri)
+    app.state.db = client["restaurante_db"]
+    print(">> Mongo conectado")
+    yield
+    print(">> Cerrando conexión Mongo")
+    client.close()
 
-@app.on_event("startup")
-async def startup():
-    await startup_db()
-    app.state.db = get_db()
-
-@app.on_event("shutdown")
-async def shutdown():
-    await shutdown_db()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(ordenes_router, prefix="/ordenes", tags=["Ordenes"])
 app.include_router(resenias_router, prefix="/resenias", tags=["Resenias"])
