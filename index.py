cae19d94adc3506baa590119a0705f6fc477ac65
nicollegordillo <nicollegordillo912@gmail.com>
2025-04-23 08:38:24 -0600

    fix: direct db import

--- index.py
+++ index.py
@@ -8,18 +8,11 @@ from routes.ordenes import router as ordenes_router
 from routes.resenias import router as resenias_router
 from routes.imagenes import router as imagenes_router
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print(">> Lifespan inicializando...")
-    mongo_uri = os.environ.get("MONGODB_URI")
-    client = AsyncIOMotorClient(mongo_uri)
-    app.state.db = client["restaurante_db"]
-    print(">> Mongo conectado")
-    yield
-    print(">> Cerrando conexión Mongo")
-    client.close()
+mongo_uri = os.environ.get("MONGODB_URI")
+client = AsyncIOMotorClient(mongo_uri)
+db = client["restaurante_db"]
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.include_router(ordenes_router, prefix="/ordenes", tags=["Ordenes"])
 app.include_router(resenias_router, prefix="/resenias", tags=["Resenias"])
